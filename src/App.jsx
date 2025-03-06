aaf14a72d29e279bd640022e5885ecbd778a4956
msun97 <msun9_7@naver.com>
2025-03-06 16:12:36 +0900

    merge 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,30 @@ import Cart from './pages/mypage/cart';
 import Myposts from './pages/mypage/myposts';
 import Profile from './pages/mypage/profile';
 import Logout from './pages/mypage/logout';
+import ProductListPage from './pages/ProductList';
 const App = () => {
   return (
     <>
-   <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="/productlist" element={<ProductListPage />} />
-          <Route path='/productinquiry' element={<ProductInquiry />} />
-          <Route path="mypage" element={<MyPage />}>
-            <Route path="order" element={<Order />} />
-            <Route path="recent" element={<Recent />} />
-            <Route path="wishlist" element={<Wishlist />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="myposts" element={<Myposts />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="logout" element={<Logout />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="/productlist" element={<ProductListPage />} />
+            <Route path="/productinquiry" element={<ProductInquiry />} />
+            <Route path="mypage" element={<MyPage />}>
+              <Route path="order" element={<Order />} />
+              <Route path="recent" element={<Recent />} />
+              <Route path="wishlist" element={<Wishlist />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="myposts" element={<Myposts />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="logout" element={<Logout />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFiles />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<NotFiles />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
