1c9ff8ed91a76988c548cf73c93fa4ca21b28f0f
yejiahn1 <yeji_09@naver.com>
2025-03-06 11:21:43 +0900

    제품리스트페이지

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import GlobalStyle from './styles/GlobalStyle';
 import Layout from './components/Layout';
 import Home from './pages/home';
 import NotFiles from './pages/notfile';
+import ProductListPage from './pages/productList';
+
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />}/>
-            <Route path="/home" element={<Home />} />
-          </Route>
-          <Route path="*" element={<NotFiles />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/productlist" element={<ProductListPage />} />
+                    </Route>
+                    <Route path="*" element={<NotFiles />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 };
 
 export default App;
---
+++ src/pages/ProductList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductListPage = () => {
+    return (
+        <div className="w-full h-full relative bg-fixed bg-[url('/images/productListPageBg.png')] bg-no-repeat bg-top bg-cover">
+        
+        </div>
+    );
+};
+
+export default ProductListPage;
