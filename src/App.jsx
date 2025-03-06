02f9ee0ace2756d925cf60f6a36d02f812ecd147
peaceRyun <dbsrn1110@gmail.com>
2025-03-06 12:03:24 +0900

    상품문의페이지 추가(라우팅도 추가)

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/home';
 import NotFiles from './pages/notfile';
+import ProductInquiry from './pages/productinquiry/ProductInquiry';
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-          </Route>
-          <Route path="*" element={<NotFiles />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path='/home' element={<Home />} />
+                        <Route path='/productinquiry' element={<ProductInquiry />} />
+                    </Route>
+                    <Route path='*' element={<NotFiles />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 };
 
 export default App;
---
+++ src/pages/productinquiry/ProductInquiry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductInquiry = () => {
+    return <div className='wrap p-()'>sss</div>;
+};
+
+export default ProductInquiry;
