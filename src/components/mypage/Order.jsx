e2f48bca21cd446d89a5c271ae03437dd475a158
bybykwon <byoungyoonkwon@google.com>
2025-03-06 18:43:38 +0900

    컴포넌트 수정

--- src/components/mypage/Order.jsx
+++ src/components/mypage/Order.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import Line from './Line'
 
 const OrderList = () => {
 	return (
-		<div className="flex justify-between text-[12px]">
-		<span>주문번호</span>
-		<span>주문상품</span>
-		<span>주문가격</span>
-		<span className='underline'>취소신청</span>
-		<span className='underline'>거래완료</span>
-	</div>
+		<><div className="flex justify-between text-[12px]">
+			<span>주문번호</span>
+			<span>주문상품</span>
+			<span>주문가격</span>
+			<span className='underline'>취소신청</span>
+			<span className='underline'>거래완료</span>
+		</div><Line marginTop='30px' /></>
 	)
 }
 
