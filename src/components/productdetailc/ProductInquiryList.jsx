c632832a55683d97884497400fb7f3ac6cc5dd95
peaceRyun <dbsrn1110@gmail.com>
2025-03-07 16:55:45 +0900

    폰트 크기 조정1.

--- src/components/productdetailc/ProductInquiryList.jsx
+++ src/components/productdetailc/ProductInquiryList.jsx
@@ -6,9 +6,9 @@ const ProductInquiryList = () => {
     return (
         <div className='pt-[200px] px-[330px] flex flex-col gap-[22px]'>
             <div className='w-full flex items-center justify-between'>
-                <h2 className='font-secondary text-[44px] font-bold'>상품문의</h2>
+                <h2 className='font-secondary text-[32px] font-bold'>상품문의</h2>
                 <div className='relative'>
-                    <Button className='w-[290px] h-[80px] font-bold text-xl flex items-center !justify-between p-[30px]'>
+                    <Button className='w-[290px] h-[55px] font-bold text-xl flex items-center !justify-between p-[30px]'>
                         <span className='whitespace-nowrap'>문의하기</span>
                         <svg width='24' height='24' viewBox='0 0 48 49' fill='none' xmlns='http://www.w3.org/2000/svg'>
                             <path
