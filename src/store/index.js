e1065d03333add9f9b5c48da6726d5d25be39369
bybykwon <byoungyoonkwon@google.com>
2025-03-11 17:35:48 +0900

    병합

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authR from './modules/authSlice';
 import paginationR from './modules/paginationSlice';
 import modalR from './modules/modalSlice';
+import productInquiryR from './modules/productInquirySlice';
 
 export const store = configureStore({
   reducer: {
     authR,
 		modalR,
 		paginationR,
+    productInquiryR,
   },
 });
