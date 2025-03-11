e1fdc196287077f102f545f526d4212dc5f36697
to-flatwhite1 <yeji_09@naver.com>
2025-03-11 16:03:44 +0900

    add to cart 연결결

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authR from './modules/authSlice';
-import paginationR from './modules/paginationSlice';
-import productInquiryR from './modules/productInquirySlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authR from "./modules/authSlice";
+import paginationR from "./modules/paginationSlice";
+import productInquiryR from "./modules/productInquirySlice";
+import cartR from "./modules/cartSlice";
 
 export const store = configureStore({
-    reducer: {
-        authR,
-        paginationR,
-        productInquiryR,
-    },
+  reducer: {
+    authR,
+    paginationR,
+    productInquiryR,
+    cartR,
+  },
 });
