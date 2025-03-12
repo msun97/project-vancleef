9e0abed4d1c13835dc9a8e1fbe37f464a0fe0ac9
bybykwon <byoungyoonkwon@google.com>
2025-03-12 10:49:35 +0900

    로그인 회원가입 스타일 수정 및 버튼 라우터연결

--- src/components/authentication/LoginModal.jsx
+++ src/components/authentication/LoginModal.jsx
@@ -3,12 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { closeModal } from '../../store/modules/modalSlice';
 import Input from '../input';
 import Button from '../button';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function LoginFull() {
   const dispatch = useDispatch();
   const isOpen = useSelector((state) => state.modalR.isOpen);
-
+  const navigate = useNavigate();
 
   return (
     <div
@@ -20,12 +20,12 @@ function LoginFull() {
       }}
     >
       <div className="bg-white p-6 relative w-[580px] h-full ml-[600px]">
-        <button
+        {/* <button
           onClick={() => Navigate(-1)}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
         >
           닫기
-        </button>
+        </button> */}
         {/* 추가 기능이 필요하면 여기에 작성 */}
 			<div className="container flex flex-col justify-center items-center mt-[127px] space-y-[20px]">
 					<h1 className='pb-[86px]'>
@@ -36,12 +36,12 @@ function LoginFull() {
 	            />
 	          </h1>
 
-					<Input className='w-[360px] h-[55px] font-bold text-gray text-center' placeholder='아이디/이메일을 입력해 주세요.'/>
-					<Input className='w-[360px] h-[55px] text-gray text-center font-bold  mb-[78px]' placeholder='비밀번호를 입력해 주세요.'/>
-					<Button variant='primary' className='w-[355px] h-[55px] font-bold mb-[30px]' >로그인</Button>
-					<Button variant='secondary' className='w-[355px] h-[55px] font-bold'>카카오 로그인</Button>
-				<div className='flex flex-row gap-[50px] p-[33px]'>	<button>아이디 찾기</button>|<button>비밀번호 찾기</button></div>
-					<Button variant='secondary'  className='w-[355px] h-[55px]'>회원가입</Button>
+					<Input className='w-[360px] h-[55px] font-bold text-[#9C9C9C] text-center' placeholder='아이디/이메일을 입력해 주세요.'/>
+					<Input className='w-[360px] h-[55px] text-center text-[#9C9C9C] font-bold  mb-[78px]' placeholder='비밀번호를 입력해 주세요.'/>
+					<Button variant='primary' className='w-[355px] h-[55px] !font-bold mb-[30px]' >로그인</Button>
+					<Button variant='secondary' className='w-[355px] h-[55px] !font-bold'>카카오 로그인</Button>
+				<div className='flex flex-row gap-[50px] p-[33px] !font-bold'>	<button>아이디 찾기</button>|<button>비밀번호 찾기</button></div>
+					<Button variant='secondary'  className='w-[355px] h-[55px] !font-bold' onClick={() => navigate('/signup')}>회원가입</Button>
 			</div>
       </div>
     </div>
--- src/components/mypage/MypostsModal.jsx
+++ src/components/mypage/MypostsModal.jsx
@@ -27,8 +27,8 @@ const MypostsModal = () => {
   };
 
   return (
-    <Draggable nodeRef={nodeRef} bounds="body" handle=".handle">
-      <div className='fixed inset-0'>
+    <Draggable nodeRef={nodeRef} bounds="" handle=".handle">
+      <div className='fixed inset-0 over'>
       	<div ref={nodeRef} className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-[710px] min-h-[626px] z-[1000] mt-[80px] mx-auto p-[50px] bg-white rounded-md border border-black">
 	        {/* 드래그 가능한 영역을 표시하기 위해 상단에 handle 클래스 추가 */}
 	        <div className="handle cursor-move mb-4">
@@ -52,7 +52,7 @@ const MypostsModal = () => {
 	
 	        <textarea 
 	          rows={1} 
-	          className="w-full p-2 bg-[#F4F4F4] focus:outline-none focus:ring-1 focus:ring-gray-200 text-[#9C9C9C]" 
+	          className="w-full p-2 bg-[#F4F4F4] focus:outline-none focus:ring-1 focus:ring-gray-200 placeholder-[#9C9C9C] text-[#000]" 
 	          placeholder="제목"
 									style={{ resize: 'none' }}
 	        />
@@ -61,7 +61,7 @@ const MypostsModal = () => {
 	            rows={4}
 	placeholder="내용"
 							style={{ resize: 'none' }}
-	            className="w-full p-2 bg-[#F4F4F4] text-[#9C9C9C] focus:outline-none focus:ring-1 focus:ring-gray-200"
+	            className="w-full p-2 bg-[#F4F4F4] placeholder-[#9C9C9C] text-[#000] focus:outline-none focus:ring-1 focus:ring-gray-200"
 	          />
 	        </div>
 	
