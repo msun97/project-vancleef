13ca261719e529efcb8b7401bf020f6fc61d5aec
msun97 <godqhr9782@naver.com>
2025-03-12 23:07:21 +0900

    오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import NotFiles from "./pages/notfile";
 import MyPage from "./pages/mypage";
 import Order from "./pages/mypage/order";
