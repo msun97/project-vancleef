ee55a6eccfa24c3b2ad72da81a857f03afd89c57
yejiahn1 <yeji_09@naver.com>
2025-03-14 15:49:11 +0900

    상품 리스트 메뉴 카테고리별 필터링

--- src/components/product/ProductListItem.jsx
+++ src/components/product/ProductListItem.jsx
@@ -1,30 +1,23 @@
-import React, { useState } from "react";
-import ProductImg from "./ProductImg";
-import ProductDescription from "./ProductDescription";
-import { useSelector } from "react-redux";
+import ProductImg from './ProductImg';
+import ProductDescription from './ProductDescription';
 
 const ProductListItem = ({ className, productdata }) => {
-  const product = useSelector((state) => state.productR.productdata); // 전체 상품 데이터
-  console.log(productdata);
-
-  return (
-    <div className={`product-item ${className}`}>
-      <div className="py-2.5 text-left text-[#1c1c1c]">
-        {/* products 배열을 map을 사용하여 반복 */}
-
-        <div key={productdata.productid}>
-          <ProductImg
-            src={productdata.objectimage[0]} // 첫 번째 이미지 가져오기
-            width={500}
-            alt={productdata.title}
-            title={productdata.title}
-            className="rounded-t-[900px] h-[391px] w-full bg-[#F1F1F1] object-contain"
-          />
-          <ProductDescription productdata={productdata} />
+    return (
+        <div className={`product-item ${className}`}>
+            <div className="py-2.5 text-left text-[#1c1c1c]">
+                <div key={productdata.productid}>
+                    <ProductImg
+                        src={productdata.objectimage[0]} // 첫 번째 이미지 가져오기
+                        width={500}
+                        alt={productdata.title}
+                        title={productdata.title}
+                        className="rounded-t-[900px] h-[391px] w-full bg-[#F1F1F1] object-contain"
+                    />
+                    <ProductDescription productdata={productdata} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductListItem;
