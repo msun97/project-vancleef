2d2e40f70ed3fa6fa77fb5d8cb9eec182c930b94
bybykwon <byoungyoonkwon@google.com>
2025-03-19 18:32:17 +0900

    주문내역 수정

--- src/components/mypage/Order.jsx
+++ src/components/mypage/Order.jsx
@@ -1,25 +1,71 @@
-import React from 'react'
-import Line from './Line'
-import { useDispatch } from 'react-redux'
-import { openModal } from '../../store/modules/modalSlice'
-import CancelOrderModal from './CancelOrderModal'
-
-
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../store/modules/modalSlice';
+import CancelOrderModal from './CancelOrderModal';
 
 const OrderList = () => {
-	const dispatch = useDispatch();
-	
-	return (
-	<div className="flex justify-between text-[12px] p-[20px]">
-			<span>주문번호</span>
-			<span>주문상품</span>
-			<span>주문가격</span>
-			<span className='underline'  onClick={() => dispatch(openModal())}>취소신청</span>
-			<CancelOrderModal/>
-			<span className='underline'>거래완료</span>
-		</div>
-	)
-}
+  const dispatch = useDispatch();
+  const [completePurchase, setCompletePurchase] = useState(null);
 
-export default OrderList
+  // localStorage에서 completePurchase 객체 읽어오기
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.completePurchase) {
+      setCompletePurchase(currentUser.completePurchase);
+    }
+  }, []);
+
+  // completePurchase가 없으면 메시지 출력
+  if (!completePurchase) {
+    return (
+      <div className="p-[20px] text-[12px]">
+        구매완료된 내역이 없습니다.
+      </div>
+    );
+  }
+
+  return (
+    <div className="border-b border-gray-300 p-[20px]">
+      <div className="flex justify-between items-center text-[12px]">
+        {/* 주문번호 */}
+     <div>
+     	   <div>주문번호:</div> <span>{completePurchase.deliverNumber}</span>
+	
+     </div>
+        {/* 주문상품: deliverItem은 중첩 배열로 되어있으므로 두 번의 map 사용 */}
+        <span>
+          주문상품:
+          {completePurchase.deliverItem &&
+            completePurchase.deliverItem.map((group, idx) => (
+              <div key={idx}>
+                {group.map((item, i) => (
+                  <div key={i}>{item.title}</div>
+                ))}
+              </div>
+            ))}
+        </span>
+
+        {/* 주문가격 */}
+<div>
+	        <div>  주문가격:</div>
+	        <span>
+					KRW{' '}{completePurchase.sumPrice &&
+	            completePurchase.sumPrice
+	              .toString()
+	              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+	        </span>
+</div>
+
+        {/* 취소신청 버튼 */}
+        <span className="underline" onClick={() => dispatch(openModal())}>
+          취소신청
+        </span>
+        <CancelOrderModal />
+        {/* 거래완료 텍스트 */}
+        <span className="underline">거래완료</span>
+      </div>
+    </div>
+  );
+};
+
+export default OrderList;
